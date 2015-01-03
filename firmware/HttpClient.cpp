3712dc2e1036801e0303cab51588e01b9db0cbbe
jgaull <Jonbeesh@gmail.com>
2015-01-03 03:38:30 -0800

    Update HttpClient.cpp

--- firmware/HttpClient.cpp
+++ firmware/HttpClient.cpp
@@ -99,7 +99,7 @@ void HttpClient::request(http_request_t &aRequest, http_response_t &aResponse, h
     client.print(aHttpMethod);
     client.print(" ");
     client.print(aRequest.path);
-    client.print(" HTTP/1.0\r\n");
+    client.print(" HTTP/1.1\r\n");
 
     #ifdef LOGGING
     Serial.println("HttpClient>\tStart of HTTP Request.");
